1e9b8c8ad1216188a9813edd9932723c79658dcb
tobias-piotr <piotr.tobiasz.dev@gmail.com>
2024-01-26 14:03:48 +0100

    Add echo server code

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"os"
+
+	"starling/cmd/server"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	if err := run(); err != nil {
+		slog.Error("Failed to start the app", "error", err)
+	}
+}
+
+func run() error {
+	options := server.Options{
+		Logger: getLogger(),
+	}
+	return server.Run(&options)
+}
+
+func getLogger() *slog.Logger {
+	if os.Getenv("DEBUG") != "true" {
+		return slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	}
+	return slog.Default()
 }
