4ebbcf835eff45662f5fd0fa0deab357c57dbff6
tobias-piotr <piotr.tobiasz.dev@gmail.com>
2024-02-14 09:19:13 +0100

    Add get endpoint

--- trips/repositories.go
+++ trips/repositories.go
@@ -2,5 +2,6 @@ package trips
 
 type TripRepository interface {
 	Create(data *TripData) (*Trip, error)
-	GetAll(page int, perPage int) ([]*Trip, error)
+	GetAll(page int, perPage int) ([]*TripOverview, error)
+	Get(id string) (*Trip, error)
 }
